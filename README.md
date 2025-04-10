bf52171c90e5f5ba3e963b99ab54402269e27d55
HaiWei-Seedr <david@hdsmoto.com>
2025-04-10 15:37:46 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# line-rate-alert
-LINE Bot for monitoring USD exchange rate
+# LINE 匯率提醒 Bot
+以 Flask 部署的 LINE Messaging API Webhook，準備結合匯率爬蟲與群組推播。
---
+++ main.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+@app.route("/")
+def home():
+    return "LINE Bot is running."
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"你說了：{msg}")
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
